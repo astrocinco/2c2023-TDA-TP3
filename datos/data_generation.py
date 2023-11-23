972c93af54bbe70709adba40797cb8c5a0abc55d
cmtFIUBA <cteszkiewicz@fi.uba.ar>
2023-11-23 19:21:31 -0300

    multiplesdata_sets

--- datos/data_generation.py
+++ datos/data_generation.py
@@ -8,8 +8,8 @@ def crear_subconjuntos_b(cantidad_b, cantidad_a, archivo_salida):
     for linea in listado_jugadores.readlines():
         jugadores_posibles.append(linea[:-1])
 
-    for i in range(0,cantidad_a):
-        j = random.randint(0, cantidad_a-1-i)
+    for i in range(0,45-cantidad_a):
+        j = random.randint(0, 45-i)
         jugadores_posibles.pop(j)
 
     salida = open(archivo_salida, 'w')
@@ -19,7 +19,8 @@ def crear_subconjuntos_b(cantidad_b, cantidad_a, archivo_salida):
         jug = random.randint(0, cantidad_a-2)
         
         for h in range(0,jug):
-            index = random.randint(0,cantidad_a-1)
+            index = random.randint(0,cantidad_a-2)
+            print(f"cantidad a = {cantidad_a} index = {index}, len = {len(jugadores_posibles)}")
             subconjunto.add(jugadores_posibles[index]) 
         cadena = ''
         for s in subconjunto:
@@ -31,6 +32,13 @@ def crear_subconjuntos_b(cantidad_b, cantidad_a, archivo_salida):
 
         salida.write(cadena)
 
-    
+def crear_varios_data_sets(nomenclatura: str, cantidad_data, b_inicial, incremento):
+    b_actual = b_inicial
+    for i in range(0,cantidad_data): 
+        nombre = nomenclatura + '_' + str(b_actual) + '.txt'
+        crear_subconjuntos_b(b_actual, random.randint(1,42), nombre)
+        b_actual += incremento
+
+crear_varios_data_sets('prueba1', 10, 4, 2)
 
 
