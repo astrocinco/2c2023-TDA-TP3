fb319767586020fa938bc0e2001d984398915a3d
Santiago <84828241+astrocinco@users.noreply.github.com>
2023-11-24 00:42:57 -0300

    Added: sets grandes

--- datos/listado_cleaner.py
+++ datos/listado_cleaner.py
@@ -19,9 +19,19 @@ def write_clear_listado_completo(set_jugadores):
     file.close()
 
 
+def minimum_players_line_cleaner(file, minimum):
+    with open(file, "r") as f:
+        lines = f.readlines()
+    with open(file, "w") as f:
+        for line in lines:
+            if len(line.split(",")) > minimum:
+                f.write(line)
+
+
 if __name__ == "__main__":
-    set_jugadores, lista_jugadores = lista_listado_completo()
-    print(set_jugadores)
+    #set_jugadores, lista_jugadores = lista_listado_completo()
+    #print(set_jugadores)
     #print(lista_jugadores)
     #print("Largos:", len(set_jugadores), len(lista_jugadores))
-    write_clear_listado_completo(set_jugadores)
+    #write_clear_listado_completo(set_jugadores)
+    minimum_players_line_cleaner("pruebaLanger_5000.txt", 3)
