c95ca315a983665e945090f9f2ddaf8ebbd9c56e
cmtFIUBA <cteszkiewicz@fi.uba.ar>
2023-11-24 00:09:21 -0300

    mejoras sets

--- datos/data_generation.py
+++ datos/data_generation.py
@@ -14,27 +14,36 @@ def crear_subconjuntos_b(cantidad_b, cantidad_a, archivo_salida):
     cantidad_a = min(cantidad_a,max_jugadores)
     
     #restrinjo aleatoriamente
-    restringir = random.randint(1,2)
+    restringir = random.randint(1,7)
     if restringir == 1:
-        for i in range(cantidad_b,max_jugadores-cantidad_a):
+        max_posible = max_jugadores-cantidad_a
+        for i in range(cantidad_b,max_posible):
             j = random.randint(0, max_jugadores-i)
-            jugadores_posibles.pop(j)
 
     salida = open(archivo_salida, 'w')
     
     #creo los subconjuntos con los jugadores aleatorios
     for k in range(0,cantidad_b):
         subconjunto = set()
-        jug = random.randint(0, cantidad_a-1)
+        jug = random.randint(1, cantidad_a-1)
    
+        #hago que sean poco probables los subconjuntos pequeños
         if jug < 3:
-            if jug == 0:
-                tirar_de_vuelta = 2
-            else:
-                tirar_de_vuelta = random.randint(1,10)
+            tirar_de_vuelta = random.randint(1,5)
             if tirar_de_vuelta !=1:
                 while jug < 3:
-                    jug = random.randint(0, cantidad_a-1)
+                    jug = random.randint(1, cantidad_a-1)
+        else:
+
+            #hago que sean poco probables los subconjuntos muy grandes
+            if jug > 6:
+                if jug > 15:
+                    tirar_de_vuelta = random.randint(1,20)
+                else:
+                    tirar_de_vuelta = random.randint(1,5)
+                if tirar_de_vuelta !=1:
+                    while jug > 15:
+                        jug = random.randint(1, cantidad_a-1)
    
         for h in range(0,jug):
             index = random.randint(0,cantidad_a-1)
@@ -51,13 +60,13 @@ def crear_subconjuntos_b(cantidad_b, cantidad_a, archivo_salida):
 
         salida.write(cadena)
 
-def crear_varios_data_sets(nomenclatura: str, cantidad_data, b_inicial, incremento):
+def crear_varios_data_sets(nomenclatura: str, cantidad_data, b_inicial, incremento, cantidad_a_min = 2, cantidad_a_max = 200):
     b_actual = b_inicial
     for i in range(0,cantidad_data): 
         nombre = nomenclatura + '_' + str(b_actual) + '.txt'
-        crear_subconjuntos_b(b_actual, random.randint(1,46), nombre)
+        crear_subconjuntos_b(b_actual, random.randint(cantidad_a_min,cantidad_a_max), nombre)
         b_actual += incremento
 
-crear_varios_data_sets('prueba4', 30, 104, 3)
+crear_varios_data_sets('prueba4', 50, 100, 2, 5)
 
 
